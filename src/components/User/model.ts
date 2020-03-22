5020eceb19c4399cfe27836d50634fb3cf67e06d
Ya_Myn <ricky.month@gmail.com>
2020-03-22 15:01:02 +0200

    feat(typeorm): create typeORM conection

--- src/components/User/model.ts
+++ src/components/User/model.ts
@@ -1,36 +1,75 @@
-import { Document, Schema } from 'mongoose';
-import * as connections from '../../config/connection';
-import { AggregationCursor } from 'mongodb';
+// import { Document, Schema } from 'mongoose';
+// import * as connections from '../../config/connection';
+// import { AggregationCursor } from 'mongodb';
 
-export interface IUserModel extends Document {
-    fullname: string;
+// export interface IUserModel extends Document {
+//     fullname: string;
+//     email: string;
+// }
+
+// export interface IStatModel extends AggregationCursor {
+//     _id: number;
+//     number: number;
+// }
+
+// const UserSchema: Schema = new Schema(
+//     {
+//         fullName: {
+//             type: String,
+//             trim: true,
+//         },
+//         email: {
+//             type: String,
+//             required: true,
+//             index: true,
+//             unique: true,
+//         },
+//     },
+
+//     {
+//         timestamps: true,
+//         collection: 'usermodels',
+//         versionKey: false,
+//     },
+// );
+
+// export default connections.db.model<IUserModel>('UserModel', UserSchema);
+import { connections } from '../../config/connection';
+import { AggregationCursor } from 'mongodb';
+import {
+    createConnection,
+    Connection,
+    Entity,
+    ObjectID,
+    ObjectIdColumn,
+    Column,
+    getMongoRepository,
+    MongoRepository,
+} from 'typeorm';
+
+@Entity()
+export class User {
+    @ObjectIdColumn()
+    id: ObjectID;
+
+    @Column()
+    fullName: string;
+
+    @Column({
+        unique: true,
+    })
     email: string;
 }
+// const UserModel: MongoRepository =
 
-export interface IStatModel extends AggregationCursor {
-    _id: number;
-    number: number;
-}
+export default async (): Promise<MongoRepository<User>> => {
+    const connection: Connection = await connections();
+    const UserModel: MongoRepository<User> = connection.getMongoRepository(
+        User,
+    );
 
-const UserSchema: Schema = new Schema(
-    {
-        fullName: {
-            type: String,
-            trim: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            index: true,
-            unique: true,
-        },
-    },
+    return UserModel;
+};
 
-    {
-        timestamps: true,
-        collection: 'usermodels',
-        versionKey: false,
-    },
-);
-
-export default connections.db.model<IUserModel>('UserModel', UserSchema);
+// MongoRepository<User>
+// export default connections.getMongoRepository(User);
--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,34 +1,13 @@
-// const mongoose = require('mongoose');
-import * as mongoose from 'mongoose';
+import { createConnection, Connection } from 'typeorm';
 
-interface IConnectOptions {
-    autoReconnect: boolean;
-    reconnectTries: number; // Never stop trying to reconnect
-    reconnectInterval: number;
-    useNewUrlParser?: boolean;
-    useCreateIndex?: boolean;
-    useUnifiedTopology?: boolean;
+export async function connections(): Promise<Connection> {
+    const connection: Connection = await createConnection({
+        type: 'mongodb',
+        host: 'localhost',
+        port: 27017,
+        database: 'users_db',
+        entities: ['../components/User/model.ts'],
+    });
+
+    return connection;
 }
-
-const MONGODB_URI: string = 'mongodb://localhost:27017/';
-const MONGODB_DB_MAIN: string = 'users_db';
-const MONGO_URI: string = `${MONGODB_URI}${MONGODB_DB_MAIN}`;
-
-const connectOptions: IConnectOptions = {
-    // automatically try to reconnect when it loses connection
-    autoReconnect: true,
-    // reconnect every reconnectInterval milliseconds
-    // for reconnectTries times
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000,
-    // flag to allow users to fall back to the old
-    // parser if they find a bug in the new parse
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-};
-
-export const db: mongoose.Connection = mongoose.createConnection(
-    MONGO_URI,
-    connectOptions,
-);
